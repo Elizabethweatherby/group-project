b0abc7ea7ce9887e63f7c3ca69ccb0d51cd6f0fc
Elizabethweatherby <elizabethwarren2792@gmail.com>
2023-07-12 20:31:36 -0500

    saving for later

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var map = tt.map({
+    key: 'AtAM1lDPRKeXpA9gXH60pqycYvLoKQAm',
+    container: 'map',
+
+});
+map.addControl(new tt.FullscreenControl());
+map.addControl(new tt.NavigationControl());
