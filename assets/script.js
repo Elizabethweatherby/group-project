c5a0f862899565bddcaa25b10b462174652cf07f
ch40s1 <zaratedaniel67@gmail.com>
2023-07-18 12:19:54 -0500

    search now takes different input, and added new search criteria

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,25 @@
 const apiKey = "https://www.thecocktaildb.com/api/json/v2/9973533/"
 const byName = "search.php?s=";
 const byFirstLetter = "search.php?f=";
-const byIngredient = 'filter.php?i';
-const searchInput = document.getElementById('searchInput');
-const glassesList = 'http://www.thecocktaildb.com/api/json/v2/9973533/list.php?g=list'
-const categoryList = 'http://www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list';
-const ingredientList = 'http://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list';
-const alcoholFilter = 'http://www.thecocktaildb.com/api/json/v2/9973533/list.php?a=list';
+const byIngredient = 'filter.php?i=';
+const glassesList = 'https://www.thecocktaildb.com/api/json/v2/9973533/list.php?g=list'
+const categoryList = 'https://www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list';
+const ingredientList = 'https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list';
+const alcoholFilter = 'https://www.thecocktaildb.com/api/json/v2/9973533/list.php?a=list';
 const popularDrinks = 'https://www.thecocktaildb.com/api/json/v2/9973533/popular.php';
-const latestDrinks = 'https//www.thecocktaildb.com/api/json/v2/9973533/latest.php';
-const randomDrinks = 'https//www.thecocktaildb.com/api/json/v2/9973533/randomselection.php';
+const latestDrinks = 'https://www.thecocktaildb.com/api/json/v2/9973533/latest.php';
+const randomDrinks = 'https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php';
 let popularDrinkList = document.getElementById('popularDrinks');
+let latestDrinkList = document.getElementById('latestDrinks');
+let randomDrinkList = document.getElementById('randomDrinks');
 // Event listener for the search button click
 const searchForm = document.getElementById('searchForm');
 searchForm.addEventListener('submit', searchCocktails);
 
-// List most latest cocktails (only available to $2+ Patreon supporters)
-// www.thecocktaildb.com/api/json/v1/1/latest.php
-
-// List Popular cocktails (only available to $2+ Patreon supporters)
-// www.thecocktaildb.com/api/json/v1/1/popular.php
-// Lookup a selection of 10 random cocktails (only available to $2+ Patreon supporters)
-// www.thecocktaildb.com/api/json/v1/1/randomselection.php
-
-const searchCriteria = `${apiKey}${byIngredient}${searchInput.value}`;
 function searchCocktails(event) {
   event.preventDefault();
+  const searchInput = document.getElementById('searchInput');
+  const searchCriteria = `${apiKey}${byIngredient}${searchInput.value}`;
   fetch(searchCriteria)
     .then(response => response.json())
     .then(data => {
@@ -35,18 +29,44 @@ function searchCocktails(event) {
       console.error(error);
     });
 }
+// this gets fetches the data from all 3 at the same time
+Promise.all([
+  fetch(popularDrinks).then(response => response.json()),
+  fetch(latestDrinks).then(response => response.json()),
+  fetch(randomDrinks).then(response => response.json())
+])
+  .then(([popularData, latestData, randomData]) => {
+    // Display popular drinks
+    let popularList = document.getElementById('popularDrinks');
+    for (let i = 0; i < 10; i++) {
+      let drinkList = document.createElement('li');
+      drinkList.textContent = popularData.drinks[i].strDrink;
+      popularList.appendChild(drinkList);
+    }
 
-fetch(popularDrinks)
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  data.drinks.forEach(drink => {
-    let drinkList = document.createElement('li');
-    drinkList.textContent = drink.strDrink;
-    popularDrinkList.appendChild(drinkList);
+    // Display latest drinks
+    let latestList = document.getElementById('latestDrinks');
+    //uses for.. of loop to iterate over data and assign it to drink variable on each iteration
+    for (let drink of latestData.drinks) {
+      let drinkList = document.createElement('li');
+      drinkList.textContent = drink.strDrink;
+      latestList.appendChild(drinkList);
+    }
+
+    // Display random drinks
+    let randomList = document.getElementById('randomDrinks');
+    for (let drink of randomData.drinks) {
+      let drinkList = document.createElement('li');
+      drinkList.textContent = drink.strDrink;
+      randomList.appendChild(drinkList);
+    }
+  })
+  .catch(error => {
+    console.error(error);
   });
-})
-.catch(error => {
-  console.error(error);
-});
+
+
+
+
+
 
